f3073fff9af846584239f9cd1574280c8e37eabf
Viktoriya Koval <vrkoval@gmail.com>
2023-04-19 16:24:07 +0300

    t3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+const refs = { 
+  form: document.querySelector(".form")
+}
+console.dir(refs.form);
+refs.form.addEventListener('submit', e => {
+  e.preventDefault()
+  const { delay, step, amount } = e.currentTarget;
+  const delayNum = Number(delay.value)
+  let stepNum = delayNum
+  const amountNum = Number(amount.value)
+  for (let i = 1; i <= amountNum; i += 1) {
+
+      createPromise(i, stepNum)
+          .then(({ position, delay }) => {
+              Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          })
+          .catch(({ position, delay }) => {
+              Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
+          stepNum += Number(step.value)
+  }
+})
+// ---------------------------------------------------------------------------------------------------------
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (shouldResolve) {
+            resolve({ position, delay })
+        }
+        reject({ position, delay });
+    }, delay)
+})
 }
